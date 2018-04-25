67349370e19f67f1123a32b80a20f59bfc652d81
Peter Kuriyama <ptrkrym@uw.edu>
2018-04-25 10:12:10 -0700

    add working script

---
+++ mtheory_main.R
@@ -0,0 +1,6 @@
+#----------------------------------------------------------------------------------------
+#Set working directory and load packages
+setwd("C://Users//Peter//Dropbox//postdoc//calcofi")
+library(devtools)
+devtools::create("mtheory")
+
