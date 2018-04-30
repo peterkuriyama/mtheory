fe9d5ba62e1781d8804592a0d0942c0f1c23bbbc
peterkuriyama <ptrkrym@uw.edu>
2018-04-30 11:08:53 -0700

    update script

--- mtheory_main.R
+++ mtheory_main.R
@@ -3,8 +3,6 @@
 setwd("C://Users//Peter//Dropbox//postdoc//calcofi")
 setwd("/Users/peterkuriyama/Dropbox/postdoc/calcofi/mtheory")
 
-load_all()
-
 library(devtools)
 library(deSolve)
 library(parallel)
@@ -13,6 +11,7 @@ library(tidyverse)
 library(rEDM)
 library(reshape2)
 
+load_all()
 #----------------------------------------------------------------------------------------
 #Simulate three species time series
 
@@ -23,12 +22,39 @@ library(reshape2)
 #     ylab = "Forecast Skill (rho)")
 ##
 
+#Generate data for three species
 samps <- generate_data(seed = 500, nsamples = 12, 
-  state = c(X = 1, Y = 1, Z = 1))
+  state = c(X = 1, Y = 1, Z = 1), times = seq(1, 1000, by = 1))
+#samps are formatted this way to facilitate plotting in ggplot
+
+#Convert the samples to a list and sample 100 values
+sample_ts <- split(samps$samps, f = samps$samps$par)
+
+sample_ts <- lapply(sample_ts, FUN = function(xx){
+  tt <- dcast(xx, pars + time ~ variable, value.var = 'value')
+  tt <- tt[order(tt$time), ]
+  # tt$time <- tt$time * 100
+  #Multiply time by 100 and sample every 100 values
+  # tt <- tt[seq(1, 100000, by = 100), ]
+})
+
+
+#Conduct simplex on each list
+
+
+simplex_out <- lapply(c("AbvBioAnnProd", "noh020tot", "invrichness"), function(var) {
+    simplex(composite_ts[, c("Year", var)], E = 2:4, lib = composite_lib, pred = composite_pred)
+})
+#Sample every 100 values because the time steps are currently too fine
+
+
 
 #Apply simplex to sampled data
 temp <- samps$samps %>% filter(pars == 1)
 temp <- dcast(temp, pars + time ~ variable, value.var = 'value')
+
+
+
 
 #sample every 1000 values
 temp <- temp[seq(1, 100000, by = 100), ]
