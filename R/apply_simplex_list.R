15761890580243916ec9061b87143b6b5105b5ea
peterkuriyama <ptrkrym@uw.edu>
2018-04-30 15:54:59 -0700

    add apply simplex list function

---
+++ R/apply_simplex_list.R
@@ -0,0 +1,19 @@
+#' Apply simplex list function
+#' Function to apply simplex to list of sampled data for values of X, Y, and Z
+
+#' @param E Embedding dimensions
+#' @param lib Index of library values
+#' @param pred Index of prediction values
+#' @param samp_ts Output from sample_data function
+
+#' @export
+
+apply_simplex_list <- function(E, lib, pred, samp_ts){
+  ss <- lapply(samp_ts, FUN = function(yy){
+    #Apply simplex to elements of the list
+    temp <- apply_simplex(E = E, lib = lib, pred = cpred,
+      dat_in = yy)
+    return(temp)
+  })
+  return(ss)
+}
