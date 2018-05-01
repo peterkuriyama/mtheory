7c7680efb486a930312de7664e46f62b4a387587
peterkuriyama <ptrkrym@uw.edu>
2018-05-01 15:37:38 -0700

    update working script

--- mtheory_main.R
+++ mtheory_main.R
@@ -10,8 +10,11 @@ library(plyr)
 library(tidyverse)
 library(rEDM)
 library(reshape2)
+library(doParallel)
 
-load_all()
+devtools::install_github('peterkuriyama/mtheory')
+library(mtheory)
+# load_all()
 #----------------------------------------------------------------------------------------
 #Simulate three species time series
 
@@ -22,75 +25,60 @@ load_all()
 #     ylab = "Forecast Skill (rho)")
 ##
 
+#-----------------------
 #Generate data for three species
 samps <- generate_data(seed = 500, nsamples = 12, 
-  state = c(X = 1, Y = 1, Z = 1), times = seq(1, 1000, by = 1))
+  state = c(X = 1, Y = 1, Z = 1), times = seq(1, 5000, by = 1))
 #samps are formatted this way to facilitate plotting in ggplot
 
-#Convert the samples to a list and sample 100 values
-sample_ts <- split(samps$samps, f = samps$samps$par)
+#-----------------------
+#Sample data at some frequency
+sample_ts <- sample_data(data_in = samps, samp_freq = 10)
 
-sample_ts <- lapply(sample_ts, FUN = function(xx){
-  tt <- dcast(xx, pars + time ~ variable, value.var = 'value')
-  tt <- tt[order(tt$time), ]
-  # tt$time <- tt$time * 100
-  #Multiply time by 100 and sample every 100 values
-  # tt <- tt[seq(1, 100000, by = 100), ]
-})
+#-----------------------
+#Use simplex to find best embedding dimension
+simplex_list <- apply_simplex_list(E = 2:8, lib = c(1, 100), pred = c(1, 100), 
+  samp_ts = sample_ts)
 
 
-#Conduct simplex on each list
+#-----------------------
+#Function to 
 
 
-simplex_out <- lapply(c("AbvBioAnnProd", "noh020tot", "invrichness"), function(var) {
-    simplex(composite_ts[, c("Year", var)], E = 2:4, lib = composite_lib, pred = composite_pred)
-})
-#Sample every 100 values because the time steps are currently too fine
+ctl <- mtheory_ctl(seed = 500, nsamples = 12, state = c(X = 1, Y = 1, Z = 1), 
+  times = seq(1, 5000, by = 1), samp_freq = 10, E = 2:8, lib = c(1, 100),
+  pred = c(1, 100))
 
 
 
-#Apply simplex to sampled data
-temp <- samps$samps %>% filter(pars == 1)
-temp <- dcast(temp, pars + time ~ variable, value.var = 'value')
+run_simplex(ctl_in = ctl, ncores = 6)
+
+#-----------------------
+#Identify nonlinearity
 
 
 
+#-------------------------------------------------------------------
+#Plot simplex_list values
+sl1 <- ldply(simplex_list)
+names(sl1)[1] <- 'iter'
 
-#sample every 1000 values
-temp <- temp[seq(1, 100000, by = 100), ]
-lib <- c(1, 100)
-pred <- c(801, 900)
+sl1 %>% ggplot() + geom_line(aes(colour = variable, x = E, y = rho)) +
+  facet_wrap(~ iter) + ylim(c(0, 1))
 
-tt <- simplex(temp$Y, lib, pred, stats_only = FALSE)
-tt
+ldply
+
+samp_freq <- 50 #sample every 50 values to start
+
+
+#To do
+####Detect chaos in time series
+
 
 #----------------------------------------------------------------------------------------
 #Plot the time series in ggplot
 samps$samps  %>% ggplot(aes(x = time, y = value)) +
     geom_line(aes(colour = variable)) + facet_wrap(~ pars, scales = 'free') 
-samps$samps$time <- samps$samps$time * 100
-
-
-samps$samps 
-samps
-
-
-lib <- c(1, 100)
-pred <- c(901, 1000)
-
-simplex_output <- simplex(samps$samps %>% filter(pars == 1), lib, pred, stats_only = FALSE)
-simplex_output <- simplex(ts, lib, pred)
-
-
 
 #----------------------------------------------------------------------------------------
-#Write the parallelization with dopar
 generate_data(nsamples = 6, seed = 301, ncores = 6)
-
-
-ind <- 2
-out <- samps[[ind]]
-par(oma = c(0, 0, 3, 0))
-plot(out, xlab = "time", ylab = "-")
-plot(out[, "X"], out[, "Z"], pch = ".")
-
