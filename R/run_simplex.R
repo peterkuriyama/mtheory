fb10c273ed8ae763908b6907a118faf39d88eb56
Peter Kuriyama <pkuriyama@gmail.com>
2018-05-02 11:04:26 -0700

    remove commented code

--- R/run_simplex.R
+++ R/run_simplex.R
@@ -8,33 +8,9 @@
 #' @export
 
 run_simplex <- function(ctl_in, ncores = 6){
-
-  #----------------------------------------------
-  #Create filename for looking up
-  # timez <- paste0("times_", paste(range(ctl_in$times), collapse = "_"))
-  # seedz <- paste0("seed", ctl_in$seed)
-  # statez <- paste0("state", paste(ctl_in$state, collapse = "_"))
-  # sampz <- paste0('nsamples', ctl_in$nsamples)
-  # filename <- paste0("dat_", timez, "_", seedz, "_", statez, "_",
-  #   sampz, ".Rdata")
-  
-  # #----------------------------------------------
-  # #Generate data if not already present
-  # if(length(grep(filename, list.files("output"))) == 0){
-  #   samps <- generate_data(seed = ctl_in$seed, nsamples = ctl_in$nsamples,
-  #     state = ctl_in$state, times = ctl_in$times, ncores = ncores)
-  # }
-
-  # #If present just load the data
-  # if(length(grep(filename, list.files("output"))) != 0){
-  #   load(paste0('output//', filename))
-  #   samps <- outs
-  #   rm(outs)  
-  # }
-  # filename <- paste0("output//", filename)
-# browser()
   
   samps <- load_dat(ctl_in, ncores = ncores)
+
   #----------------------------------------------
   #sample data at some frequency
   sample_ts <- sample_data(data_in = samps, samp_freq = ctl_in$samp_freq)
