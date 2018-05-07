4f470f40985a2ef35043364000c706b6dbbb94c2
peterkuriyama <ptrkrym@uw.edu>
2018-05-07 16:11:46 -0700

    option to add error

--- R/mtheory_ctl.R
+++ R/mtheory_ctl.R
@@ -9,11 +9,13 @@
 #' @param E Dimensionality of simplex
 #' @param lib Index of library values for simplex
 #' @param pred Index of prediction values for simplex
+#' @param error_sd Standard deviation of added error; default is 0
 
 #' @export
 
-mtheory_ctl <- function(seed, nsamples, state, times, samp_freq, E, lib, pred){
+mtheory_ctl <- function(seed, nsamples, state, times, samp_freq, E, lib, pred,
+  error_sd = 0){
   out <- list(seed = seed, nsamples = nsamples, state = state, times = times, 
-    samp_freq = samp_freq, E = E, lib = lib, pred = pred)
+    samp_freq = samp_freq, E = E, lib = lib, pred = pred, error_sd = error_sd)
   return(out)
 }
